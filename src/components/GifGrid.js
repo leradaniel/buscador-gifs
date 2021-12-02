79410e15a25ddde9de3751e4e968e4f18646e400
Daniel Lera <lera.danielm@gmail.com>
2021-12-02 12:17:28 -0300

    Add func: Componente GifGridItem que crea las imagenes segun una url recibida

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([])
+  //Estado que va a almacenar la información de las imágenes recibidas:
+  const [images, setimages] = useState([]);
   //Al poner el arreglo de dependencias vacío, solamente se llama una vez al contenido:
   useEffect(() => {
     getGifs();
@@ -10,8 +12,6 @@ export const GifGrid = ({ category }) => {
   //Función asíncrona para hacer peticiones HTTP y traer los gifs.
   //Con el Search Endpoint de la API de Giphy: https://developers.giphy.com/docs/api/endpoint#search
   const getGifs = async () => {
-
-
     //Endpoint de una búsqueda:
     const url =
       "https://api.giphy.com/v1/gifs/search?q=Kimetsu+no+Yaiba&limit=10&api_key=K4yeBra8OiNy7EuURHUin81fZA1TBjoi";
@@ -25,20 +25,24 @@ export const GifGrid = ({ category }) => {
       return {
         id: img.id,
         title: img.title,
-        url: img.images?.downsized_medium.url
+        url: img.images?.downsized_medium.url,
       };
     });
-    setImages(gifs);
+    //Se guardan las imagenes en un estado:
+    setimages(gifs);
   };
 
   return (
     <div>
       <h3>{category}</h3>
-      <ol>
-        {images.map((img) => {
-          return <li key={img.id}>{img.title}</li>;
-        })}
-      </ol>
+      {/* Por cada imagen en el estado, se crea un componente: */}
+      {images.map((img) => {
+        //Se podrían enviar cada una de las propiedades con un spread:
+        //Esto permite que no mandemos la img enter.
+        //Tampoco que tengamos que mandar cada propiedad por separado.
+        //O sea, title= img.title, url = img.url, etc:
+        return <GifGridItem key={img.id} {...img} />;
+      })}
     </div>
   );
 };
